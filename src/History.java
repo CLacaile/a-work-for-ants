740dfb51b8ae372dd80a7c07e975d2c26960c3ad
CLacaile <clementlacaile@gmail.com>
2018-10-16 16:21:02 +0200

    changed list type int to Integer

--- src/History.java
+++ src/History.java
@@ -1,9 +1,11 @@
+import Relevance;
+
 public abstract class History {
 
 	/**
 	 * A list of agent thresholds throughout time
 	 */
-	private List<List<int>> thresholds;
+	private List<List<Integer>> thresholds;
 
 	private List<Relevance> relevances;
 
