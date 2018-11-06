5dbdb522d42540f06bbb50c754fedaf8180fba5e
CLacaile <clementlacaile@gmail.com>
2018-11-06 18:09:40 +0100

    updated test main
    Signed-off-by: CLacaile <clementlacaile@gmail.com>

--- src/Main.java
+++ src/Main.java
@@ -3,8 +3,14 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("Début du test");
-        Agent agent1 = new Agent();
-        System.out.println(agent1.getId());
+        int nbTasks = 3;
+        Agent agent1 = new Agent(1, nbTasks);
+        System.out.println("Id de l'agent: " + agent1.getId());
+        System.out.println("Seuils des taches");
+        for(int i=0; i<nbTasks; i++) {
+            System.out.println("Tache " + i + " : " + " " + agent1.getThreshold(i));
+        }
+        agent1.getThreshold(1);
         System.out.println("Fin du test");
     }
 }
