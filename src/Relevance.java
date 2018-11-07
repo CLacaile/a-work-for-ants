4597f2f4b5d366a7db49a3781c5c454bad28746d
CLacaile <clementlacaile@gmail.com>
2018-11-07 20:11:44 +0100

    formatted text
    Signed-off-by: CLacaile <clementlacaile@gmail.com>

--- src/Relevance.java
+++ src/Relevance.java
@@ -2,56 +2,48 @@ import java.util.ArrayList;
 
 public class Relevance {
 
+	// Attributes
 	private ArrayList<Float> relevance = new ArrayList<>();
-
 	private int min;
-
 	private int max;
-
 	private int seed;
-
 	private boolean periodical;
 
+	// Constructor
 	public Relevance(float relevanceSum) {
 		float random = Simulation.randomIntInRange(0, (int) Math.ceil(relevanceSum*100));
 		random /= 100;
 		relevance.add(random);
 	}
 
-	public void init_relevance(int min, int max, int seed, boolean periodical) {
-
-	}
-
-	public void update_relevance() {
-
-	}
-
+	// Getters
 	public ArrayList<Float> getRelevance() {
 		return relevance;
-	}
-
-	public void setRelevance(ArrayList<Float> relevance) {
-		this.relevance = relevance;
 	}
 
 	public int getMin() {
 		return min;
 	}
 
-	public void setMin(int min) {
-		this.min = min;
-	}
-
 	public int getMax() {
 		return max;
 	}
 
-	public void setMax(int max) {
-		this.max = max;
-	}
-
 	public int getSeed() {
 		return seed;
+	}
+
+	// Setters
+	public void setRelevance(ArrayList<Float> relevance) {
+		this.relevance = relevance;
+	}
+
+	public void setMin(int min) {
+		this.min = min;
+	}
+
+	public void setMax(int max) {
+		this.max = max;
 	}
 
 	public void setSeed(int seed) {
@@ -65,4 +57,15 @@ public class Relevance {
 	public void setPeriodical(boolean periodical) {
 		this.periodical = periodical;
 	}
+
+	// Methods
+	public void init_relevance(int min, int max, int seed, boolean periodical) {
+
+	}
+
+	public void update_relevance() {
+
+	}
+
+
 }
