060872bc730c9da3614a222fbe3f87d9a35fb4c8
CLacaile <clementlacaile@gmail.com>
2018-11-07 20:09:07 +0100

    formatted text
    Signed-off-by: CLacaile <clementlacaile@gmail.com>

--- src/Task.java
+++ src/Task.java
@@ -2,45 +2,44 @@ import java.util.List;
 
 public class Task {
 
+	// Attributes
 	private int id;
-
 	private Relevance relevance;
-
 	private List<Integer> agents;
 
+	// Constructor
 	public Task(int id, float relevanceSum) {
 		this.id = id;
 		relevance = new Relevance(relevanceSum);
 	}
 
+	// Getters
 	public int getId() {
 		return id;
-	}
-
-	public void setId(int id) {
-		this.id = id;
 	}
 
 	public Relevance getRelevance() {
 		return relevance;
 	}
 
-	public void setRelevance(Relevance relevance) {
-		this.relevance = relevance;
-	}
-
 	public List<Integer> getAgents() {
 		return agents;
+	}
+
+	// Setters
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public void setRelevance(Relevance relevance) {
+		this.relevance = relevance;
 	}
 
 	public void setAgents(List<Integer> agents) {
 		this.agents = agents;
 	}
 
-	public void Task() {
-
-	}
-
+	// Methods
 	/**
 	 * Change la relevance selon le nb d'agent qui ont perform la tache
 	 */
