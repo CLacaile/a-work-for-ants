5fa37fbe5ca2373031fcb034d12a043e54e3d873
CLacaile <clementlacaile@gmail.com>
2018-11-26 22:10:53 +0100

    added runSimulation function
    Signed-off-by: CLacaile <clementlacaile@gmail.com>

--- src/Synchronous.java
+++ src/Synchronous.java
@@ -1,7 +1,13 @@
-public abstract class Synchronous extends Simulation {
+public class Synchronous extends Simulation {
 
-	public void run() {
-
+	public void runSimulation() {
+		while(relevanceSum > (new Float(0.01))) {
+			for (int i = 0; i < numberOfIteration; i++) {
+				for (int j = 0; j < total_agent_number; j++) {
+					agents.get(j).performPickedTask();
+				}
+			}
+			computeRelevanceSum();
+		}
 	}
-
 }
