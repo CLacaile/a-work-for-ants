384222c77663e9ae9f4cccac95d8fd2bf8adddf8
CLacaile <clementlacaile@gmail.com>
2018-12-01 10:37:15 +0100

    added mgmt of history in the simulation instead of the main
    Signed-off-by: CLacaile <clementlacaile@gmail.com>

--- src/Synchronous.java
+++ src/Synchronous.java
@@ -5,6 +5,8 @@ public class Synchronous extends Simulation {
 			System.out.println("--------------- Iteration #"+i+" : ---------------");
 			for(int j=0; j<this.total_agent_number; j++) {
 				this.agents.get(j).liveLife(this.tasks);
+				this.history.addThresholdsToHistory(this.agents.get(j));
+				this.history.addRelevanceToHistory(this.agents.get(j).getPickedTask());
 			}
 			this.computeRelevanceSum();
 			System.out.println("Relevance sum: "+this.getRelevanceSum());
